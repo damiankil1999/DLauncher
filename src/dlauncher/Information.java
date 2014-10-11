11db5e1d885b9fab7401dfbc4930591fbdfb9ff9
damiankil1999 <damiankil1999@users.noreply.github.com>
2014-10-11 17:15:54 +0200

    Everything
    changed names

---
+++ src/dlauncher/Information.java
@@ -0,0 +1,35 @@
+package dlauncher;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+/**
+ * @author damian
+ */
+public class Information {
+
+   private final String version = "Alpha 0.0.0.1";
+   private final String newsLink = "http://www.missnecraft.net/";
+   private final String defaultIcon = "/images/test.png";
+
+   public Information() {
+      Logger.getGlobal().log(Level.INFO, "Launcher Version " + version + " Loaded!");
+   }
+
+   public String getVersion() {
+      return version;
+   }
+
+   public String getNewsLink(){
+      return newsLink;
+   }
+
+   public void setIcon(JFrame frame) {
+      frame.setIconImage(frame.getToolkit().getImage(getClass().getResource(this.defaultIcon)));
+   }
+
+   public void setIcon(JFrame frame, String img) {
+      frame.setIconImage(frame.getToolkit().getImage(getClass().getResource("/images/" + img)));
+   }
+}
---
+++ src/dlauncher/dialogs/TimeTingys.java
@@ -0,0 +1,65 @@
+/* 
+ * Hey Hey nice that you are looking for Eastereggs,
+ * Keep on looking for it!
+ */
+package dlauncher.dialogs;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+import javax.swing.JPasswordField;
+
+/**
+ * @author damian
+ */
+public class TimeTingys {
+   private final JPasswordField pfield;
+   //Password field:
+   private final char defaultchar = '\u220e'; // ∎
+   private final char xmaschar1 = '\u2603'; // ☃
+   private final char xmaschar2 = '\u2603';
+   //
+
+   private String getDate() {
+      Date date = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("dd-MM");
+      sdf.setTimeZone(TimeZone.getDefault());
+      String formattedDate = sdf.format(date);
+      return formattedDate;
+   }
+
+   private String getDate1() {
+      Date date = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+      sdf.setTimeZone(TimeZone.getDefault());
+      String formattedDate = sdf.format(date);
+      return formattedDate;
+   }
+
+   private String getTime() {
+      Date date = new Date();
+      SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+      sdf.setTimeZone(TimeZone.getDefault());
+      String formattedTime = sdf.format(date);
+      return formattedTime;
+   }
+   
+   
+   public TimeTingys(JPasswordField field) {
+      this.pfield = field;
+   }
+   
+   public void setup(){
+      JPasswordField field = this.pfield;
+      
+      switch (getDate()) {
+         case "25-12":
+            field.setEchoChar(xmaschar1);
+            break;
+         case "26-12":
+            field.setEchoChar(xmaschar2);
+            break;
+      }
+   }
+
+}
