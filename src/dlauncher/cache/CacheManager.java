f0e631d53b001c8bc85aa8a00e15820e2826073f
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2014-10-18 20:19:12 +0200

    Updated interface CacheManager

--- src/dlauncher/cache/CacheManager.java
+++ src/dlauncher/cache/CacheManager.java
@@ -1,12 +1,15 @@
 package dlauncher.cache;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URL;
 import javax.swing.JLabel;
 
 public interface CacheManager {
 
-    public void downloadURL(URL url, OutputStream to);
+    public void downloadURL(URL url, OutputStream to) throws IOException;
+    
+    public byte[] downloadURL(URL url) throws IOException;
 
     public void addToCache(URL url, byte[] file);
 
